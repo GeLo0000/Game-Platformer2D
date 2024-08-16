3064397230ed9d17a9dc913ed84272439c24a614
Oleg Baranov <baranov.off2@gmail.com>
2024-08-16 22:47:18 +0300

    Add 2 Lvl

--- Platformer2D/Assets/Scripts/PlayerMovement2D.cs
+++ Platformer2D/Assets/Scripts/PlayerMovement2D.cs
@@ -57,7 +57,7 @@ public class PlayerMovement2D : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Physics2D.OverlapCircle(groundCheck.position, 0.9f, whatIsGround))
+        if (Physics2D.OverlapCircle(groundCheck.position, 0.7f, whatIsGround))
         {
             isGrounded = true;
             animator.SetBool("IsJumping", false);
